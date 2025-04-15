c98de7f1dc40c9adf05006883c9d9835eba04f7d
Salma-Mahmoudd <salmaaamahmouddd@gmail.com>
2025-04-15 18:12:18 +0200

    feat: add `CustomPipe` and integrate with template and `AppComponent`

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,21 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from "./components/header/header.component";
+import { HeaderComponent } from './components/header/header.component';
 import { CustomDirective } from './directives/custom.directive';
 import { CommonModule } from '@angular/common';
+import { CustomPipe } from './pipes/custom.pipe';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, CustomDirective, CommonModule],
+  imports: [
+    RouterOutlet,
+    HeaderComponent,
+    CustomDirective,
+    CommonModule,
+    CustomPipe,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'angular-labs';
---
+++ src/app/pipes/custom.pipe.ts
@@ -0,0 +1,10 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'custom',
+})
+export class CustomPipe implements PipeTransform {
+  transform(value: string, ...args: string[]): unknown {
+    return value.split('').reverse().join(args[0]);
+  }
+}
